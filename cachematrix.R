2373c0d5058feffb9c0dcef51fd77f68b518d16b
Iris <31424034+irisshvartsman@users.noreply.github.com>
2019-08-16 13:44:12 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y){
+                x <<- y
+                inv <<- NULL
+                
+        }
+        get <- function(){x
+                          
+                
+                }
+        
 
 }
 
